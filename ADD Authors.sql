043afac74c2d62c573ede2fc58a8112fb82654b5
Koulinn <drdverzola@gmail.com>
2021-08-23 15:20:18 +0100

    updated Author date

--- ADD Authors.sql
+++ ADD Authors.sql
@@ -1,83 +1,84 @@
+-- DATE FORMAT yyyy-mm-dd
 INSERT INTO authors(
     name,
     last_name,
     country,
-    birth_year,
+    birth_year
     ) VALUES(
         'Dan',
         'Brown',
         'UK',
-        '1974'
+        '1974-10-10'
         );
 
 INSERT INTO authors(
     name,
     last_name,
     country,
-    birth_year,
+    birth_year
     ) VALUES(
         'Agatha',
         'Christie',
         'UK',
-        '1976'
+        '1976-10-10'
         );
 
 INSERT INTO authors(
     name,
     last_name,
     country,
-    birth_year,
+    birth_year
     ) VALUES(
         'Paulo',
         'Coelho',
         'BR',
-        '1963'
+        '1963-10-10'
         );
 
 INSERT INTO authors(
     name,
     last_name,
     country,
-    birth_year,
+    birth_year
     ) VALUES(
         'Ken',
         'Follett',
         'Germany',
-        '1958'
+        '1958-10-10'
         );
 
 INSERT INTO authors(
     name,
     last_name,
     country,
-    birth_year,
+    birth_year
     ) VALUES(
         'John',
         'Grisham',
         'US',
-        '1971'
+        '1971-10-10'
         );
 
 INSERT INTO authors(
     name,
     last_name,
     country,
-    birth_year,
+    birth_year
     ) VALUES(
         'Haruki',
         'Murakami',
         'JP',
-        '1928'
+        '1928-10-10'
         );
 
 INSERT INTO authors(
     name,
     last_name,
     country,
-    birth_year,
+    birth_year
     ) VALUES(
         'James',
         'Patterson',
         'UK',
-        '1985'
+        '1985-10-10'
         );
