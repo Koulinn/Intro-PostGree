745d4c3047eed71ed6da2287b912ac14232b8cfb
Koulinn <drdverzola@gmail.com>
2021-08-23 13:40:02 +0100

    created both tables

---
+++ CREATE AUTHORS.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS public.authors CASCADE;
+
+CREATE TABLE
+    IF NOT EXISTS
+        authors (
+            author_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            name VARCHAR ( 80 ) NOT NULL,
+            last_name VARCHAR ( 50 ) NOT NULL,
+            country VARCHAR ( 50 ) NOT NULL,
+            birth_Year VARCHAR ( 50 ) NOT NULL,
+);
---
+++ CREATE BOOKS.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS public.books CASCADE;
+
+CREATE TABLE
+    IF NOT EXISTS
+        books (
+            book_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+            name VARCHAR ( 80 ) NOT NULL,
+            author VARCHAR ( 50 ) NOT NULL,
+            category VARCHAR ( 50 ) NOT NULL,
+            cover VARCHAR ( 50 ) NOT NULL,
+            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            updated_at DATE DEFAULT CURRENT_DATE(), 
+            published_at TIMESTAMPTZ NOT NULL DEFAULT NOW() 
+);
